25324be97d62cf01de17178997a6ac7fe68b922d
ashley <ashleypak71@yahoo.com>
2022-09-13 15:56:37 +0100

    Added custom user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # replace username with an email
+    username = models.EmailField(
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="Required. You need an email address to log in.",
+        error_messages={"unique": "A user with that email already exists."},
+    )
