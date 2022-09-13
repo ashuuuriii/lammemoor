de7d99ccf2f0231d0a35e2fe6e96fc2f2518c453
ashley <ashleypak71@yahoo.com>
2022-09-13 20:59:47 +0100

    Added Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import HomeScreen from "./screens/HomeScreen";
 
@@ -9,13 +10,14 @@ function App() {
   return (
     <Router>
       <Header />
-      <main className="py-3">
+      <main className="py-5">
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </Container>
       </main>
+      <Footer />
     </Router>
   );
 }
