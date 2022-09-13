2a3068f6c163ee17af0c2c625efe37bf993f8568
ashley <ashleypak71@yahoo.com>
2022-09-13 20:59:34 +0100

    Added top page content to index page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Row, Col, Container, Image, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+import fashion from "../assets/images/f6998.jpg";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1 className="display-1">HomeScreen</h1>
-    </div>
+    <Container className="pt-4">
+      <Row className="justify-content-center align-items-center">
+        <Col className="md-5 text-center text-md-start">
+          <h1>
+            <div className="display-2">Lammermoor</div>
+            <div className="display-5 text-muted">
+              Sewing patterns of the Victorian and Edwardian periods.
+            </div>
+          </h1>
+          <p className="lead my-4 text-muted">
+            Historical costuming made simple. Your one stop shop for Victorian
+            era costuming.
+          </p>
+          <LinkContainer to="/shop">
+            <Button className="btn btn-secondary btn-lg mb-4">Shop Now</Button>
+          </LinkContainer>
+        </Col>
+        <Col className="col-md-5 text-center">
+          <Image
+            src={fashion}
+            className="fluid"
+            alt="Fashion, Journal de Demoiselles, 1858."
+          ></Image>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
