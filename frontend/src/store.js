f664f2756faf089186a02760b71fc8ec1c00c13c
ashley <ashleypak71@yahoo.com>
2022-09-14 18:45:10 +0100

    Added userLoginReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    userLogin: userLoginReducer,
+  },
   devTools: true,
   preloadedState: {},
 });
