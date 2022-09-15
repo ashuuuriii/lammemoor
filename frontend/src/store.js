1ff3a666acf1360c37d2670244b28ec1dcaefebc
ashley <ashleypak71@yahoo.com>
2022-09-15 09:28:09 +0100

    Added preloaded state to store to track user logins

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userLoginReducer } from "./reducers/userReducers";
 
+const preloadedUserInfo = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const store = configureStore({
   reducer: {
     userLogin: userLoginReducer,
   },
   devTools: true,
-  preloadedState: {},
+  preloadedState: {
+    userLogin: {userInfo: preloadedUserInfo},
+  },
 });
 
 export default store;
