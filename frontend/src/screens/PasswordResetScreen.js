1924b730267749a37a56d247e944e0f8b0239b20
ashley <ashleypak71@yahoo.com>
2022-09-16 21:10:12 +0100

    Added sendPasswordToken dispatch to PasswordResetScreen

--- frontend/src/screens/PasswordResetScreen.js
+++ frontend/src/screens/PasswordResetScreen.js
@@ -1,22 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import FormContainer from "../components/FormContainer";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { sendPasswordToken } from "../actions/userActions";
+import { USER_PASSWORD_RESET_RESET } from "../constants/userConstants";
 
 const PasswordResetScreen = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // TODO: dispatch password reset action here
+    dispatch(sendPasswordToken(email));
   };
+
+  const userPasswordReset = useSelector((state) => state.userPasswordReset);
+  const { loading, success, error } = userPasswordReset;
+
+  useEffect(() => {
+    if (success) {
+      dispatch({ type: USER_PASSWORD_RESET_RESET });
+      navigate("/accounts/password_reset/reset/");
+    }
+  }, [navigate, success]);
 
   return (
     <FormContainer>
       <h1>Password Reset</h1>
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
       <p>
         Forgot your password? Please enter your email below and we'll send you a
         link to create a new password via email.
