945929d8f8e69652a193f8c45b39f2a8ed5dca84
ashley <ashleypak71@yahoo.com>
2022-09-16 17:30:39 +0100

    Added registration action to make api call to register view

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAILURE,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -48,3 +51,48 @@ export const logout = () => (dispatch) => {
     type: USER_LOGOUT,
   });
 };
+
+export const register =
+  (firstName, lastName, email, password) => async (dispatch) => {
+    try {
+      dispatch({
+        type: USER_REGISTER_REQUEST,
+      });
+
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/accounts/register",
+        {
+          first_name: firstName,
+          last_name: lastName,
+          username: email,
+          password: password,
+        },
+        config
+      );
+
+      dispatch({
+        type: USER_REGISTER_SUCCESS,
+        payload: data,
+      });
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: data,
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+      });
+    }
+  };
