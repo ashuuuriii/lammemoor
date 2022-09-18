1c9e80d57d0bb769719c67536399930dbd5f20e2
ashley <ashleypak71@yahoo.com>
2022-09-18 17:33:27 +0100

    Added Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    pdf = models.FileField(upload_to="pdfs/", null=True, blank=True)
+    category = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    n_reviews = models.IntegerField(blank=True, null=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    pdf_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    n_stock = models.IntegerField(null=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
