783dd250d7f7b3b3b0b807dc900fa327e4f1784e
ashley <ashleypak71@yahoo.com>
2022-09-19 11:54:23 +0100

    Added userReducers tests to test updated state

---
+++ frontend/src/reducers/userReducers.test.js
@@ -0,0 +1,211 @@
+import "@testing-library/jest-dom";
+
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userPasswordResetReducer,
+  userPasswordTokenReducer,
+} from "../reducers/userReducers";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAILURE,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_PASSWORD_RESET_REQUEST,
+  USER_PASSWORD_RESET_SUCCESS,
+  USER_PASSWORD_RESET_FAIL,
+  USER_PASSWORD_RESET_RESET,
+  USER_PASSWORD_TOKEN_REQUEST,
+  USER_PASSWORD_TOKEN_SUCCESS,
+  USER_PASSWORD_TOKEN_FAIL,
+  USER_PASSWORD_TOKEN_RESET,
+} from "../constants/userConstants";
+
+const initialState = { userInfo: null };
+
+describe("Test userLoginReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {};
+  });
+
+  it("test USER_LOGIN_REQUEST", () => {
+    const expectedState = { loading: true };
+
+    const reducer = userLoginReducer(state, { type: USER_LOGIN_REQUEST });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_LOGIN_SUCCESS", () => {
+    const expectedData = "success data";
+    const expectedState = { loading: false, userInfo: expectedData };
+
+    const reducer = userLoginReducer(state, {
+      type: USER_LOGIN_SUCCESS,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_LOGIN_FAILURE", () => {
+    const expectedData = "failed data";
+    const expectedState = { loading: false, error: expectedData };
+
+    const reducer = userLoginReducer(state, {
+      type: USER_LOGIN_FAILURE,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_LOGIN_REQUEST", () => {
+    const expectedState = {};
+
+    const reducer = userLoginReducer(state, { type: USER_LOGOUT });
+    expect(reducer).toEqual(expectedState);
+  });
+});
+
+describe("Test userRegisterReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {};
+  });
+
+  it("test USER_REGISTER_REQUEST", () => {
+    const expectedState = { loading: true };
+
+    const reducer = userRegisterReducer(state, { type: USER_REGISTER_REQUEST });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_REGISTER_SUCCESS", () => {
+    const expectedData = "success data";
+    const expectedState = { loading: false, userInfo: expectedData };
+
+    const reducer = userRegisterReducer(state, {
+      type: USER_REGISTER_SUCCESS,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_REGISTER_FAIL", () => {
+    const expectedData = "failed data";
+    const expectedState = { loading: false, error: expectedData };
+
+    const reducer = userRegisterReducer(state, {
+      type: USER_REGISTER_FAIL,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_LOGIN_REQUEST", () => {
+    const expectedState = {};
+
+    const reducer = userRegisterReducer(state, { type: USER_LOGOUT });
+    expect(reducer).toEqual(expectedState);
+  });
+});
+
+describe("Test userPasswordResetReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {};
+  });
+
+  it("test USER_PASSWORD_RESET_REQUEST", () => {
+    const expectedState = { loading: true };
+
+    const reducer = userPasswordResetReducer(state, {
+      type: USER_PASSWORD_RESET_REQUEST,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_RESET_SUCCESS", () => {
+    const expectedData = true;
+    const expectedState = { loading: false, success: expectedData };
+
+    const reducer = userPasswordResetReducer(state, {
+      type: USER_PASSWORD_RESET_SUCCESS,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_RESET_FAIL", () => {
+    const expectedData = "failed data";
+    const expectedState = { loading: false, error: expectedData };
+
+    const reducer = userPasswordResetReducer(state, {
+      type: USER_PASSWORD_RESET_FAIL,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_RESET_RESET", () => {
+    const expectedState = {};
+
+    const reducer = userPasswordResetReducer(state, {
+      type: USER_PASSWORD_RESET_RESET,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+});
+
+describe("Test userPasswordTokenReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {};
+  });
+
+  it("test USER_PASSWORD_TOKEN_REQUEST", () => {
+    const expectedState = { loading: true };
+
+    const reducer = userPasswordTokenReducer(state, {
+      type: USER_PASSWORD_TOKEN_REQUEST,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_TOKEN_SUCCESS", () => {
+    const expectedData = true;
+    const expectedState = { loading: false, success: expectedData };
+
+    const reducer = userPasswordTokenReducer(state, {
+      type: USER_PASSWORD_TOKEN_SUCCESS,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_TOKEN_FAIL", () => {
+    const expectedData = "failed data";
+    const expectedState = { loading: false, error: expectedData };
+
+    const reducer = userPasswordTokenReducer(state, {
+      type: USER_PASSWORD_TOKEN_FAIL,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test USER_PASSWORD_TOKEN_RESET", () => {
+    const expectedState = {};
+
+    const reducer = userPasswordTokenReducer(state, {
+      type: USER_PASSWORD_TOKEN_RESET,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+});
