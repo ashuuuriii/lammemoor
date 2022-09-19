c8eabb4882220fdf85d2804e60b65256a29a4e23
ashley <ashleypak71@yahoo.com>
2022-09-19 21:33:55 +0100

    Added getProductsList action to make API call and a filter function to optionally hide items

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Row, Button, Offcanvas, Form } from "react-bootstrap";
+import { Row, Col, Button, Offcanvas, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
+import { getProductsList } from "../actions/productActions";
+import ProductCard from "../components/ProductCard";
+import { PRODUCT_CATEGORIES } from "../constants/productConstants";
 
 const ProductListScreen = () => {
   const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products } = productList;
 
   const [showCanvas, setShowCanvas] = useState(false);
   const [checkedItems, setCheckItems] = useState([]);
@@ -15,14 +20,16 @@ const ProductListScreen = () => {
   const handleFilteredItems = (e) => {
     const { checked, id } = e.target;
     if (checked) {
-      setCheckItems([...checkedItems, id]);
+      setCheckItems([...checkedItems, PRODUCT_CATEGORIES[id]]);
     } else {
-      setCheckItems(checkedItems.filter((item) => item !== id));
+      setCheckItems(
+        checkedItems.filter((item) => item !== PRODUCT_CATEGORIES[id])
+      );
     }
   };
 
   useEffect(() => {
-    // dispatch product api call here
+    dispatch(getProductsList());
   }, [dispatch]);
 
   return (
@@ -35,27 +42,34 @@ const ProductListScreen = () => {
           </Button>
         </div>
       </Row>
-      <Row></Row>
+      <Row>
+        {checkedItems.length > 0
+          ? products.map((product) =>
+              checkedItems.indexOf(product.category) >= 0 ? (
+                <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                  <ProductCard product={product} />
+                </Col>
+              ) : null
+            )
+          : products.map((product) => (
+              <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                <ProductCard product={product} />
+              </Col>
+            ))}
+      </Row>
       <Offcanvas show={showCanvas} onHide={handleCloseCanvas}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Filter</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Form>
-            {[
-              "1840s",
-              "1850s",
-              "1860s",
-              "1870s",
-              "1880s",
-              "1890s",
-              "Edwardian",
-            ].map((cat) => (
+            {Object.keys(PRODUCT_CATEGORIES).map((cat) => (
               <Form.Check
                 type="checkbox"
                 id={cat}
                 key={cat}
                 label={cat}
+                checked={checkedItems.indexOf(PRODUCT_CATEGORIES[cat]) >= 0}
                 onChange={handleFilteredItems}
               />
             ))}
