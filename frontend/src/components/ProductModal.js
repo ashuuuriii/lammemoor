8900c5b4e4014355c219f6f982c98716a7dc2c37
ashley <ashleypak71@yahoo.com>
2022-09-19 21:32:07 +0100

    Added ProductModel component for quick view option on products page

---
+++ frontend/src/components/ProductModal.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Modal, Button, Form, Row } from "react-bootstrap";
+
+import CartAdder from "./CartAdder";
+
+import "./ProductModal.css";
+
+const ProductModal = ({ product }) => {
+  const [show, setShow] = useState(false);
+
+  const handleOpen = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      <Button onClick={handleOpen} className="quickview-btn">
+        Quick View
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <h2>{product.name}</h2>
+        </Modal.Header>
+        <Modal.Body>
+          {product.pdf_price ? (
+            <p>
+              <strong>
+                ${product.pdf_price} - ${product.price}
+              </strong>
+            </p>
+          ) : (
+            <p>${product.price}</p>
+          )}
+          <p>{product.description}</p>
+          <h3>Purchase Options</h3>
+          <CartAdder productId={product.id} />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default ProductModal;
