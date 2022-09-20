01b9003f674934dc15bd13ab7dcd660ca35041f0
ashley <ashleypak71@yahoo.com>
2022-09-20 11:26:24 +0100

    Added productDetailsReducer with tests

--- frontend/src/reducers/productReducers.test.js
+++ frontend/src/reducers/productReducers.test.js
@@ -1,11 +1,14 @@
 import "@testing-library/jest-dom";
 
-import { productListReducer } from "./productReducers";
+import { productListReducer, productDetailsReducer } from "./productReducers";
 
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 const initialState = { userInfo: null };
@@ -41,6 +44,45 @@ describe("Test productListReducer", () => {
 
     const reducer = productListReducer(state, {
       type: PRODUCT_LIST_FAIL,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+});
+
+describe("Test productDetailsReducer", () => {
+  let state;
+
+  beforeEach(() => {
+    state = { ...initialState, product: { reviews: []} };
+  });
+
+  it("test PRODUCT_DETAILS_REQUEST", () => {
+    const expectedState = { loading: true, ...state };
+
+    const reducer = productDetailsReducer(state, {
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test PRODUCT_DETAILS_SUCCESS", () => {
+    const expectedData = "success data";
+    const expectedState = { loading: false, product: expectedData };
+
+    const reducer = productDetailsReducer(state, {
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: expectedData,
+    });
+    expect(reducer).toEqual(expectedState);
+  });
+
+  it("test PRODUCT_DETAILS_FAIL", () => {
+    const expectedData = "failed data";
+    const expectedState = { loading: false, error: expectedData };
+
+    const reducer = productDetailsReducer(state, {
+      type: PRODUCT_DETAILS_FAIL,
       payload: expectedData,
     });
     expect(reducer).toEqual(expectedState);
