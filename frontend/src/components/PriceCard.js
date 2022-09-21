72c291247267eb3305fe509f827d826aee974d7a
ashley <ashleypak71@yahoo.com>
2022-09-21 21:45:03 +0100

    Added PriceCard component to display total item prices

---
+++ frontend/src/components/PriceCard.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+const PriceCard = () => {
+  const [subtotal, setSubtotal] = useState((0).toFixed(2));
+  const [vat, setVat] = useState((0).toFixed(2));
+  const [shipping, setShipping] = useState((0).toFixed(2));
+  const [total, setTotal] = useState((0).toFixed(2));
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    const tempSubtotal = cartItems.reduce(
+      (acc, item) => acc + Number(item.price) * Number(item.qty),
+      0
+    );
+    const tempVat = tempSubtotal * 0.2;
+    const tempShipping = cartItems.some((item) => item.itemType === "paper")
+      ? tempSubtotal >= 50
+        ? 0
+        : 5
+      : 0;
+    setSubtotal(tempSubtotal.toFixed(2));
+    setVat(tempVat.toFixed(2));
+    setShipping(tempShipping.toFixed(2));
+    setTotal((tempSubtotal + tempVat + tempShipping).toFixed(2));
+  }, [cartItems]);
+
+  return (
+    <Card>
+      <Card.Header>
+        <h3>Cart Totals</h3>
+      </Card.Header>
+      <Card.Body>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <strong>Subtotal:</strong> £{subtotal}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>VAT:</strong> £{vat}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Shipping:</strong> £{shipping}
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+      <Card.Footer>
+        <div className="lead">Total: </div>
+        <div>£{total}</div>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default PriceCard;
