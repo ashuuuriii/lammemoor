324747c04a8d3465708f8c7c0c5e17d4bbcd4837
ashley <ashleypak71@yahoo.com>
2022-09-22 21:35:29 +0100

    Added updateUserDetails dispatch to ManageAccountScreen

--- frontend/src/screens/ManageAccountScreen.js
+++ frontend/src/screens/ManageAccountScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -9,9 +9,16 @@ import {
   Button,
   Accordion,
 } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { updateUserDetails } from "../actions/userActions";
+import { USER_UPDATE_DETAILS_RESET } from "../constants/userConstants";
 
 const ManageAccountScreen = () => {
+  const dispatch = useDispatch();
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -22,6 +29,51 @@ const ManageAccountScreen = () => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userUpdateDetails = useSelector((state) => state.userUpdateDetails);
+  const { loading, success, error } = userUpdateDetails;
+
+  const validatePassword = (password) => {
+    // Password must contain at least one digit, one uppercase letter
+    // and must have a minimum length of 8 characters
+    const testStr = /^(?=.*\d)(?=.*[A-Z])(.{8,50})$/;
+    return testStr.test(password);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password && password !== confirmPassword) {
+      setMessage("The passwords you have entered do not match.");
+    } else if (password && !validatePassword(password)) {
+      setMessage(
+        "Your password must contain at least one digit, one uppercase letter, and have a minimum length of 8 characters."
+      );
+    } else {
+      dispatch(
+        updateUserDetails({
+          id: userInfo.id,
+          first_name: firstName,
+          last_name: lastName,
+          username: email,
+          email: email,
+          password: password,
+        })
+      );
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    setFirstName(userInfo.first_name);
+    setLastName(userInfo.last_name);
+    setEmail(userInfo.email);
+  }, [userInfo]);
+
+  useEffect(() => {
+    return () => {
+      dispatch({ type: USER_UPDATE_DETAILS_RESET });
+    };
+  }, [dispatch]);
+
   return (
     <Container className="pt-4">
       <h1>Your Account</h1>
@@ -29,7 +81,15 @@ const ManageAccountScreen = () => {
       <Row>
         <Col md={4}>
           <h2>Edit User</h2>
-          <Form>
+          {message && <Message variant="danger">{message}</Message>}
+          {error && <Message variant="danger">{error}</Message>}
+          {success && (
+            <Message variant="success">
+              Your user details have been updated.
+            </Message>
+          )}
+          {loading && <Loader />}
+          <Form onSubmit={submitHandler}>
             <Accordion defaultActiveKey="0">
               <Accordion.Item eventKey="0">
                 <Accordion.Header>Update Name</Accordion.Header>
@@ -86,7 +146,6 @@ const ManageAccountScreen = () => {
                       }
                     >
                       <Form.Control
-                        required
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -97,7 +156,6 @@ const ManageAccountScreen = () => {
                   <Form.Group controlId="passwordConfirm">
                     <Form.Label>Confirm Password</Form.Label>
                     <Form.Control
-                      required
                       type="password"
                       value={confirmPassword}
                       onChange={(e) => setConfirmPassword(e.target.value)}
