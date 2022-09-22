e9b83abe64993d170b97267f7e821684fe4ee171
ashley <ashleypak71@yahoo.com>
2022-09-22 21:36:28 +0100

    Removed unused shipping_price field from ShippingAddress

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -13,6 +13,7 @@ class CustomUser(AbstractUser):
         error_messages={"unique": "A user with that email already exists."},
     )
 
+
 class ShippingAddress(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=150, blank=False, null=False)
@@ -22,9 +23,6 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=200, null=True, blank=True)
     postal_code = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
-    shipping_rice = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True
-    )
     in_address_book = models.BooleanField()
 
     def __str__(self):
