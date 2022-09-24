247cfefa47e472a6888efca53e5e74ac8cd1b1f4
ashley <ashleypak71@yahoo.com>
2022-09-24 14:23:38 +0100

    Added CheckoutProgress component to track order stages

---
+++ frontend/src/components/CheckoutProgress.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const CheckoutSteps = ({ step1, step2, step3 }) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <Nav.Item>
+        {step1 ? (
+          <LinkContainer to="/cart">
+            <Nav.Link>
+              Cart
+              <div className="text-center">
+                <i className="bi bi-check-circle"></i>
+              </div>
+            </Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>
+            Cart
+            <div className="text-center">
+              <i className="bi bi-circle"></i>
+            </div>
+          </Nav.Link>
+        )}
+      </Nav.Item>
+
+      <Nav.Item>
+        {step2 ? (
+          <LinkContainer to="/order">
+            <Nav.Link>
+              Shipping
+              <div className="text-center">
+                <i className="bi bi-check-circle"></i>
+              </div>
+            </Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>
+            Shipping
+            <div className="text-center">
+              <i className="bi bi-circle"></i>
+            </div>
+          </Nav.Link>
+        )}
+      </Nav.Item>
+
+      <Nav.Item>
+        {step3 ? (
+          <LinkContainer to="/order/confirm">
+            <Nav.Link className="last">
+              Confirm
+              <div className="text-center">
+                <i className="bi bi-check-circle"></i>
+              </div>
+            </Nav.Link>
+          </LinkContainer>
+        ) : (
+          <Nav.Link disabled>
+            Confirm
+            <div className="text-center">
+              <i className="bi bi-circle"></i>
+            </div>
+          </Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;
