b455dfc6312096ae65eca977af962dd70f1746ad
ashley <ashleypak71@yahoo.com>
2022-09-24 21:42:31 +0100

    Updated model and serializer to create stripe price id lookup in payment endpoint

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,4 +6,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        exclude = ["pdf", "price_id", "pdf_price_id", "user"]
