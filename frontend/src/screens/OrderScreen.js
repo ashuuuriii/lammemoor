cde22e7d14cafcfe20177dbe7a08a1297918673b
ashley <ashleypak71@yahoo.com>
2022-09-24 17:33:02 +0100

    Added select from address book feature

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,42 +1,76 @@
-import React, { useEffect } from "react";
-import { Row, Col, Card, Button, Table } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Card, Button, Table, Form } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import PriceCard from "../components/PriceCard";
 import AddressForm from "../components/AddressForm";
 import CheckoutProgress from "../components/CheckoutProgress";
+import { getUserAddresses } from "../actions/userActions";
 
 import "./OrderScreen.css";
 
 const OrderScreen = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // get state
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userShippingAddress = useSelector((state) => state.userShippingAddress);
+  const { loading, userAddresses, error } = userShippingAddress;
+
+  // toggle address book or form
+  const [showAddressBook, setShowAddressBook] = useState(false);
+  const displayAddressBook = () => setShowAddressBook(true);
+  const hideAddressBook = () => setShowAddressBook(false);
+
+  // input address by radio select from address book.
+  const addressFromStore = JSON.parse(localStorage.getItem("cartAddress"));
+  const [selectedAddress, setSelectedAddress] = useState(
+    addressFromStore ? addressFromStore.id : ""
+  );
+  const addressSelectHandler = (id) => {
+    const submitAddress = userAddresses.find((x) => x.id === id);
+    localStorage.setItem("cartAddress", JSON.stringify(submitAddress));
+    setSelectedAddress(id);
+  };
+
+  // handle submit, data is stored temporarily in localStorage before order is made.
   const continueButtonHandler = () => {
-    // click submit button in AddressForm component, triggers form submit.
     // AddressForm component is not loaded when only e-patterns are in the cart.
-    if (cartItems.some((item) => item.itemType === "paper")) {
+    // or navigate when an address has been selected from the address book.
+    // click submit button in AddressForm component, triggers form submit.
+    if (
+      !cartItems.some((item) => item.itemType === "paper") ||
+      (showAddressBook && localStorage.getItem("cartAddress"))
+    ) {
+      navigate("/order/confirm");
+    } else {
       const hiddenSubmitButton = document.querySelector(".hidden-submit-btn");
       hiddenSubmitButton.click();
       if (document.querySelector("form").checkValidity()) {
         navigate("/order/confirm");
       }
-    } else {
-      navigate("/order/confirm");
     }
   };
 
+  // useEffect.
+  // Redirect anonymous users to login.
+  // Get user addresses to display address book.
   useEffect(() => {
     if (!userInfo) {
       navigate("/login?redirect=/cart");
     }
   }, [userInfo, navigate]);
+
+  useEffect(() => {
+    dispatch(getUserAddresses());
+  }, [dispatch]);
 
   return (
     <Row className="pt-4">
@@ -51,10 +85,50 @@ const OrderScreen = () => {
         ) : null}
         {cartItems.some((item) => item.itemType === "paper") ? (
           <div>
-            <Button type="button" className="address-btn">
-              Choose from address book
-            </Button>
-            <AddressForm variant="new" showAddressBookToggle noSave noButton />
+            {showAddressBook ? (
+              <div>
+                <Button
+                  type="button"
+                  className="address-btn"
+                  onClick={hideAddressBook}
+                >
+                  Enter a different address
+                </Button>
+                <Form>
+                  {userAddresses.map((address) => (
+                    <Form.Check
+                      key={address.id}
+                      type="radio"
+                      id={`choice-${address.id}`}
+                      name="address-choice"
+                      onChange={() => addressSelectHandler(address.id)}
+                      checked={address.id === selectedAddress}
+                      label={`${address.first_name} ${address.last_name}, ${
+                        address.address
+                      }, ${address.city}, ${address.country}${
+                        address.postal_code ? " ," + address.postal_code : ""
+                      }`}
+                    />
+                  ))}
+                </Form>
+              </div>
+            ) : (
+              <div>
+                <Button
+                  type="button"
+                  className="address-btn"
+                  onClick={displayAddressBook}
+                >
+                  Choose from address book
+                </Button>
+                <AddressForm
+                  variant="new"
+                  showAddressBookToggle
+                  noSave
+                  noButton
+                />{" "}
+              </div>
+            )}
           </div>
         ) : null}
       </Col>
