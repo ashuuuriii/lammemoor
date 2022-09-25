f42a325285ae427dd81e6b76f1ecd5efe975b6ef
ashley <ashleypak71@yahoo.com>
2022-09-25 20:41:55 +0100

    Added extra fields to OrderItem and Order for use in payment backend

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -7,7 +7,9 @@ from accounts.models import ShippingAddress
 
 class Order(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
-    payment_method = models.CharField(max_length=200, null=False, blank=False)
+    subtotal_price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=False, blank=False
+    )
     shipping_price = models.DecimalField(
         max_digits=7, decimal_places=2, null=False, blank=False
     )
@@ -19,17 +21,20 @@ class Order(models.Model):
     is_delivered = models.BooleanField(default=False)
     delivered_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True)
+    shipping_address = models.ForeignKey(
+        ShippingAddress, on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
-        return str(self.createdAt)
+        return str(self.created_at)
 
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    qty = models.IntegerField(null=True, blank=True, default=0)
+    purchased_qty = models.IntegerField(null=True, blank=True, default=0)
+    type = models.CharField(max_length=200, null=False, blank=False)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
 
