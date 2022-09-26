f7c35925ab1ae1c8153f7f96e67c4cc3954329df
ashley <ashleypak71@yahoo.com>
2022-09-26 12:41:28 +0100

    Updated __str__ method to prevent None type return return raising a TypeError

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -39,4 +39,4 @@ class OrderItem(models.Model):
     image = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.name) or ""
--- backend/products/models.py
+++ backend/products/models.py
@@ -31,4 +31,4 @@ class Product(models.Model):
     pdf_price_id = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name) or ""
