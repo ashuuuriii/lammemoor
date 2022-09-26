e441cc95afc1d8db3ffd0ae1386b9892680c40b1
ashley <ashleypak71@yahoo.com>
2022-09-26 12:12:29 +0100

    Moved price calculation to backend OrderViewSet create view to prevent frontend manipulation. Decimal is set at precision 7 to for db validation with a max length of 7

--- backend/orders/tests.py
+++ backend/orders/tests.py
@@ -1,3 +1,5 @@
+import decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
@@ -26,8 +28,8 @@ class OrderViewSetTest(TestCase):
             description="Description",
             rating=3.5,
             n_reviews=5,
-            price=6.31,
-            pdf_price=3.31,
+            price=decimal.Decimal(6.31),
+            pdf_price=decimal.Decimal(3.31),
             n_stock=1,
         )
         self.product2 = Product.objects.create(
@@ -37,8 +39,19 @@ class OrderViewSetTest(TestCase):
             description="Description",
             rating=3.5,
             n_reviews=5,
-            price=6.31,
-            pdf_price=3.31,
+            price=decimal.Decimal(6.31),
+            pdf_price=decimal.Decimal(3.31),
+            n_stock=5,
+        )
+        self.product3 = Product.objects.create(
+            user=self.user,
+            name="Product Name",
+            category=1,
+            description="Description",
+            rating=3.5,
+            n_reviews=5,
+            price=decimal.Decimal(10.50),
+            pdf_price=decimal.Decimal(3.31),
             n_stock=5,
         )
         self.shipping_address = ShippingAddress.objects.create(
@@ -52,6 +65,7 @@ class OrderViewSetTest(TestCase):
             in_address_book=True,
         )
         self.client = APIClient()
+        decimal.getcontext().prec = 7
 
     def test_create_order_with_address_book(self):
         order = {
@@ -65,9 +79,6 @@ class OrderViewSetTest(TestCase):
                 "country": "country",
                 "in_address_book": True,
             },
-            "shipping_price": 5.00,
-            "total_price": 20.93,
-            "subtotal_price": 15.93,
             "order_items": [
                 {
                     "product": self.product1.pk,
@@ -106,6 +117,143 @@ class OrderViewSetTest(TestCase):
         # product2's quantity needs to be reduced based on order amount
         self.assertEqual(Product.objects.all()[0].n_stock, 1)
         self.assertEqual(Product.objects.all()[1].n_stock, 3)
+
+    def test_prices_with_shipping(self):
+        order = {
+            "shipping_address": {
+                "id": 1,
+                "first_name": "First",
+                "last_name": "Last",
+                "address": "street",
+                "city": "city",
+                "postal_code": "123 456",
+                "country": "country",
+                "in_address_book": True,
+            },
+            "order_items": [
+                {
+                    "product": self.product1.pk,
+                    "name": self.product1.name,
+                    "qty": None,
+                    "itemType": "pdf",
+                    "price": 3.31,
+                    "image": "imagelink",
+                },
+                {
+                    "product": self.product2.pk,
+                    "name": self.product2.name,
+                    "qty": 2,
+                    "itemType": "paper",
+                    "price": 12.62,
+                    "image": "imagelink",
+                },
+            ],
+        }
+
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(reverse("orders-list"), order, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        new_order_obj = Order.objects.last()
+        self.assertEqual(new_order_obj.subtotal_price, decimal.Decimal("15.93"))
+        self.assertEqual(new_order_obj.shipping_price, decimal.Decimal("5.00"))
+        self.assertEqual(new_order_obj.total_price, decimal.Decimal("20.93"))
+
+    def test_prices_no_shipping_over_free_shipping_cutoff(self):
+        order = {
+            "shipping_address": {
+                "id": 1,
+                "first_name": "First",
+                "last_name": "Last",
+                "address": "street",
+                "city": "city",
+                "postal_code": "123 456",
+                "country": "country",
+                "in_address_book": True,
+            },
+            "order_items": [
+                {
+                    "product": self.product1.pk,
+                    "name": self.product1.name,
+                    "qty": None,
+                    "itemType": "pdf",
+                    "price": 3.31,
+                    "image": "imagelink",
+                },
+                {
+                    "product": self.product2.pk,
+                    "name": self.product2.name,
+                    "qty": 2,
+                    "itemType": "paper",
+                    "price": 12.62,
+                    "image": "imagelink",
+                },
+                {
+                    "product": self.product3.pk,
+                    "name": self.product3.name,
+                    "qty": 5,
+                    "itemType": "paper",
+                    "price": 52.50,
+                    "image": "imagelink",
+                },
+            ],
+        }
+
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(reverse("orders-list"), order, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        new_order_obj = Order.objects.last()
+        self.assertEqual(new_order_obj.subtotal_price, decimal.Decimal("68.43"))
+        self.assertEqual(new_order_obj.shipping_price, decimal.Decimal("0.00"))
+        self.assertEqual(new_order_obj.total_price, decimal.Decimal("68.43"))
+
+    def test_prices_no_shipping_pdf_only(self):
+        order = {
+            "shipping_address": {
+                "id": 1,
+                "first_name": "First",
+                "last_name": "Last",
+                "address": "street",
+                "city": "city",
+                "postal_code": "123 456",
+                "country": "country",
+                "in_address_book": True,
+            },
+            "order_items": [
+                {
+                    "product": self.product1.pk,
+                    "name": self.product1.name,
+                    "qty": None,
+                    "itemType": "pdf",
+                    "price": 3.31,
+                    "image": "imagelink",
+                },
+                {
+                    "product": self.product2.pk,
+                    "name": self.product2.name,
+                    "qty": None,
+                    "itemType": "pdf",
+                    "price": 3.31,
+                    "image": "imagelink",
+                },
+            ],
+        }
+
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post(reverse("orders-list"), order, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        new_order_obj = Order.objects.last()
+        self.assertEqual(new_order_obj.subtotal_price, decimal.Decimal("6.62"))
+        self.assertEqual(new_order_obj.shipping_price, decimal.Decimal("0.00"))
+        self.assertEqual(new_order_obj.total_price, decimal.Decimal("6.62"))
 
     def test_newly_entered_address(self):
         order = {
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,5 @@
+import decimal
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
@@ -8,10 +10,32 @@ from .serializers import OrderSerializer
 from .permissions import IsStaffOrOwnerOnly
 
 
+PRICE_DECIMAL_PRECISION = 7
+FREE_SHIPPING_CUTOFF = "50.00"
+SHIPPING_PRICE = "5.00"
+
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = (IsStaffOrOwnerOnly,)
+
+    def update_order_price(self, order, order_items_prices, pdf_only):
+        order.subtotal_price = decimal.Decimal(sum(order_items_prices))
+
+        if (
+            order.subtotal_price < decimal.Decimal(FREE_SHIPPING_CUTOFF)
+            and not pdf_only
+        ):
+            order.shipping_price = decimal.Decimal(SHIPPING_PRICE)
+
+        order.total_price = decimal.Decimal(order.shipping_price + order.subtotal_price)
+
+    def get_order_item_price(self, product, item):
+        if item["itemType"] == "pdf":
+            return product.pdf_price
+        else:
+            return product.price * item["qty"]
 
     def update_product_quantity(self, product, quantity):
         product.n_stock -= quantity
@@ -39,29 +63,38 @@ class OrderViewSet(viewsets.ModelViewSet):
             shipping_address.save()
         return shipping_address
 
-    def save_order_items(self, order_items, order):
+    def create_order_items(self, order_items, order):
         # Create OrderItem objects and add Order as a foreign key
+        order_items_prices = []
+
         for item in order_items:
             product = Product.objects.get(pk=item["product"])
+            price = decimal.Decimal(self.get_order_item_price(product, item))
+
             order_item = OrderItem.objects.create(
                 product=product,
                 order=order,
                 name=item["name"],
                 purchased_qty=item["qty"],
                 type=item["itemType"],
-                price=item["price"],
+                price=price,
                 image=item["image"],
             )
 
             if order_item.type == "paper":
                 self.update_product_quantity(product, item["qty"])
 
+            order_items_prices.append(price)
+
             order_item.save()
+
+        return order_items_prices
 
     def create(self, request, *args, **kwargs):
         user = request.user
         data = request.data
         order_items = data["order_items"]
+        pdf_only = all([item["itemType"] == "pdf" for item in order_items])
 
         if not order_items:
             return Response(
@@ -73,7 +106,7 @@ class OrderViewSet(viewsets.ModelViewSet):
                 shipping_address = self.get_shipping_address_foreign_key(
                     data["shipping_address"]
                 )
-            elif not all([item["itemType"] == "pdf" for item in order_items]):
+            elif not pdf_only:
                 return Response(
                     {"detail": "No shipping address provided with physical product."},
                     status=status.HTTP_400_BAD_REQUEST,
@@ -81,14 +114,18 @@ class OrderViewSet(viewsets.ModelViewSet):
             else:
                 shipping_address = None
 
+            decimal.getcontext().prec = PRICE_DECIMAL_PRECISION
+
             order = Order.objects.create(
                 user=user,
-                subtotal_price=data["subtotal_price"],
-                shipping_price=data["shipping_price"],
-                total_price=data["total_price"],
+                subtotal_price=decimal.Decimal(0.00),
+                shipping_price=decimal.Decimal(0.00),
+                total_price=decimal.Decimal(0.00),
                 shipping_address=shipping_address,
             )
+
+            order_items_prices = self.create_order_items(order_items, order)
+            self.update_order_price(order, order_items_prices, pdf_only)
             order.save()
-            self.save_order_items(order_items, order)
 
             return Response(self.serializer_class(order, many=False).data)
