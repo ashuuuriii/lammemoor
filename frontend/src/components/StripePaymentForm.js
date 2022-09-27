e7055b8cc1dfd355cf7aa53c667d3193213199e1
ashley <ashleypak71@yahoo.com>
2022-09-27 09:18:41 +0100

    Added and configured Stripe payment form

---
+++ frontend/src/components/StripePaymentForm.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { Button, Row, Spinner } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import Message from "./Message";
+import { REACT_APP_URL } from "../constants/appConstants";
+
+const StripePaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const dispatch = useDispatch();
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [infoMessage, setInfoMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setErrorMessage("");
+    setSuccessMessage("");
+    setInfoMessage("");
+    setLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: { return_url: REACT_APP_URL },
+    });
+
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setErrorMessage(error.message);
+    } else {
+      setErrorMessage("An unexpected error occurred.");
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setErrorMessage("");
+          setSuccessMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setInfoMessage("Your Payment is processing");
+          break;
+        case "requires_payment_method":
+          setErrorMessage("Your payment was unsuccessful, please try again.");
+          break;
+        default:
+          setSuccessMessage("");
+          setErrorMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe, dispatch]);
+
+  return (
+    <form id="payment-form" onSubmit={submitHandler}>
+      {errorMessage && <Message variant="danger">{errorMessage}</Message>}
+      {successMessage && <Message variant="success">{successMessage}</Message>}
+      {infoMessage && (
+        <Message variant="info">
+          {infoMessage} <Spinner animation="grow" size="sm" />
+        </Message>
+      )}
+      <PaymentElement id="payment-element" />
+      <Row className="mx-0">
+        <Button
+          type="submit"
+          className="my-3"
+          disabled={loading || !stripe || !elements}
+        >
+          {loading ? <Spinner animation="border" size="sm" /> : "Pay now"}
+        </Button>
+      </Row>
+    </form>
+  );
+};
+
+export default StripePaymentForm;
--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,12 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Row, Col } from "react-bootstrap";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import PriceCard from "../components/PriceCard";
+import CheckoutProgress from "../components/CheckoutProgress";
+import StripePaymentForm from "../components/StripePaymentForm";
 import { STRIPE_PUBLISHABLE_KEY } from "../constants/orderContants";
+import { fetchPaymentIntent } from "../actions/orderActions";
 
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 const PaymentScreen = () => {
-  return <div className="pt-4">PaymentScreen</div>;
+  const dispatch = useDispatch();
+
+  const appearance = { theme: "stripe" };
+
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order } = orderCreate;
+
+  const orderPaymentIntent = useSelector((state) => state.orderPaymentIntent);
+  const { loading, clientSecret, error } = orderPaymentIntent;
+  console.log(clientSecret);
+
+  useEffect(() => {
+    if (order) {
+      dispatch(fetchPaymentIntent(order.id));
+    }
+  }, [dispatch, order]);
+
+  return (
+    <Container className="pt-4">
+      {loading && <Loader />}
+      {error && <Message variant="danger">{error}</Message>}
+      {clientSecret && (
+        <Row>
+          <CheckoutProgress step1 step2 step3 step4 />
+          <Col lg={7}>
+            <Elements
+              stripe={stripePromise}
+              options={{ clientSecret, appearance }}
+            >
+              <h1>Payment</h1>
+              <StripePaymentForm />
+            </Elements>
+          </Col>
+          <Col lg={4}>
+            <PriceCard />
+          </Col>
+        </Row>
+      )}
+    </Container>
+  );
 };
 
 export default PaymentScreen;
