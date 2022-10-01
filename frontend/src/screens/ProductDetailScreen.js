cab6477e3b1f46773c4d29899de6977b9211075e
ashley <ashleypak71@yahoo.com>
2022-10-01 17:57:58 +0100

    Added ListGroup and Form to Reviews section to display updated serilizer field reviews

--- frontend/src/screens/ProductDetailScreen.js
+++ frontend/src/screens/ProductDetailScreen.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { Row, Col, Image, Button } from "react-bootstrap";
-import { useParams, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Image, Button, ListGroup, Form } from "react-bootstrap";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import Loader from "../components/Loader";
 import CartAdder from "../components/CartAdder";
 import Rating from "../components/Rating";
+import Message from "../components/Message";
 import { getProductDetails } from "../actions/productActions";
 import { PRODUCT_CATEGORIES } from "../constants/productConstants";
 
@@ -13,15 +14,34 @@ const ProductDetailScreen = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [rating, setRating] = useState("0");
+  const [title, setTitle] = useState("");
+  const [comment, setComment] = useState("");
+  const [message, setMessage] = useState("");
+
   const productId = useParams().id;
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
+
+  const redirectUrl = `/login?redirect=/product/${productId}`;
+
+  const userInfo = localStorage.getItem("userInfo");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (rating === "0") {
+      setMessage("You must select a rating.");
+    } else {
+      setMessage("");
+      // add review post action dispatch here
+      console.log("dispatch");
+    }
+  };
 
   useEffect(() => {
     dispatch(getProductDetails(productId));
   }, [dispatch, productId]);
 
-  // TODO: add reviews section
   return (
     <div className="pt-4">
       <Button onClick={() => navigate(-1)}>Go back</Button>
@@ -55,13 +75,82 @@ const ProductDetailScreen = () => {
                 text={`(${product.n_reviews} customer reviews)`}
                 color={"#000"}
               />
-              <p>{product.description}</p>
+              <p className="mt-3">{product.description}</p>
               <h2>Purchase Options</h2>
               <CartAdder product={product} />
             </Col>
           </Row>
           <Row>
             <h2>Reviews</h2>
+            <ListGroup variant="flush">
+              {product.reviews.length > 0 ? (
+                product.reviews.map((review) => (
+                  <ListGroup.Item key={review.id}>
+                    <p className="lead mb-1">{review.title}</p>
+                    <p className="my-0">{review.name}</p>
+                    <Rating value={review.rating} color={"#000"} />
+                    <p className="my-0">{review.created_at.substring(0, 10)}</p>
+                    <p>{review.comment}</p>
+                  </ListGroup.Item>
+                ))
+              ) : (
+                <ListGroup.Item>
+                  <p>There are no reviews for this item yet.</p>
+                </ListGroup.Item>
+              )}
+              <ListGroup.Item>
+                <h3>Write a review</h3>
+                {message && <Message variant="danger">{message}</Message>}
+                {userInfo ? (
+                  <Form onSubmit={submitHandler}>
+                    <Form.Group controlId="rating">
+                      <Form.Label>Rating (required)</Form.Label>
+                      <Form.Select
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                      >
+                        <option value="0">Please select...</option>
+                        <option value="1">Poor</option>
+                        <option value="2">Fair</option>
+                        <option value="3">Good</option>
+                        <option value="4">Very Good</option>
+                        <option value="5">Excellent</option>
+                      </Form.Select>
+                    </Form.Group>
+                    <Form.Group controlId="title">
+                      <Form.Label>Review Title (optional)</Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="reviewBody">
+                      <Form.Label>Review (optional)</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        row="5"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Button type="submit" className="my-3 d-none d-lg-block">
+                      Submit
+                    </Button>
+                    <Row className="d-lg-none d-block mx-0">
+                      <Button type="submit" className="my-3">
+                        Submit
+                      </Button>
+                    </Row>
+                  </Form>
+                ) : (
+                  <Message variant="info">
+                    Please <Link to={redirectUrl}>login</Link> to write a
+                    review.
+                  </Message>
+                )}
+              </ListGroup.Item>
+            </ListGroup>
           </Row>
         </div>
       )}
