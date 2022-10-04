f2d6d7c156fb885f70d3b6ef585374f40a873309
ashley <ashleypak71@yahoo.com>
2022-10-04 11:19:01 +0100

    Added useSearchParam to get pagination page. Updated getProductsList to include query string with page and filter categories

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Button, Offcanvas, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 import { getProductsList } from "../actions/productActions";
 import ProductCard from "../components/ProductCard";
+import Paginator from "../components/Paginator";
 import { PRODUCT_CATEGORIES } from "../constants/productConstants";
 
 const ProductListScreen = () => {
   const dispatch = useDispatch();
+  const currPath = window.location.pathname;
+
+  const [search, setSet] = useSearchParams();
+  const pageNumber = search.get("page") ? search.get("page") : "";
+
   const productList = useSelector((state) => state.productList);
-  const { products } = productList;
+  const { products, page, pages } = productList;
 
   const [showCanvas, setShowCanvas] = useState(false);
   const [checkedItems, setCheckItems] = useState([]);
 
   const handleOpenCanvas = () => setShowCanvas(true);
   const handleCloseCanvas = () => setShowCanvas(false);
+  const handleClearButton = () => setCheckItems([]);
 
   const handleFilteredItems = (e) => {
     const { checked, id } = e.target;
@@ -29,8 +37,16 @@ const ProductListScreen = () => {
   };
 
   useEffect(() => {
-    dispatch(getProductsList());
-  }, [dispatch]);
+    let queryStr = `?page=${pageNumber}`;
+
+    if (checkedItems) {
+      for (let checked of checkedItems) {
+        queryStr += `&category=${checked}`;
+      }
+    }
+
+    dispatch(getProductsList(queryStr));
+  }, [dispatch, pageNumber, checkedItems]);
 
   return (
     <div className="pt-4">
@@ -43,20 +59,20 @@ const ProductListScreen = () => {
         </div>
       </Row>
       <Row>
-        {checkedItems.length > 0
-          ? products.map((product) =>
-              checkedItems.indexOf(product.category) >= 0 ? (
-                <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-                  <ProductCard product={product} />
-                </Col>
-              ) : null
-            )
-          : products.map((product) => (
-              <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-                <ProductCard product={product} />
-              </Col>
-            ))}
+        {typeof products != "string" ? (
+          products.map((product) => (
+            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+              <ProductCard product={product} />
+            </Col>
+          ))
+        ) : (
+          <p className="my-3">There are no products in this category yet.</p>
+        )}
       </Row>
+      <Row className="pt-5">
+        <Paginator path={currPath} page={page} pages={pages} arrows />
+      </Row>
+
       <Offcanvas show={showCanvas} onHide={handleCloseCanvas}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Filter</Offcanvas.Title>
@@ -73,6 +89,14 @@ const ProductListScreen = () => {
                 onChange={handleFilteredItems}
               />
             ))}
+            <Button
+              type="button"
+              variant="danger"
+              className="my-3"
+              onClick={handleClearButton}
+            >
+              Clear
+            </Button>
           </Form>
         </Offcanvas.Body>
       </Offcanvas>
