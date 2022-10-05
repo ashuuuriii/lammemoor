feb163de70eade20e88322e4b482fcfa6b2abeef
ashley <ashleypak71@yahoo.com>
2022-10-05 21:58:58 +0100

    Removes cart from drop down and placed cart icon on nav bar at breakpoint md

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -48,7 +48,39 @@ const Header = () => {
               Lammermoor
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="d-flex">
+            <Nav>
+              <LinkContainer to="/cart" className="d-block d-md-none mx-3">
+                <Nav.Link>
+                  {cartFull ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      class="bi bi-cart-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      class="bi bi-cart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </div>
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <LinkContainer to="/shop">
@@ -62,7 +94,7 @@ const Header = () => {
               </LinkContainer>
             </Nav>
             <Nav>
-              <LinkContainer to="/cart">
+              <LinkContainer to="/cart" className="d-none d-md-block">
                 <Nav.Link>
                   {cartFull ? (
                     <i className="bi bi-cart-fill"></i>
